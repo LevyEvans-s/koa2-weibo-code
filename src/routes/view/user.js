15f3b4bf428289b65568e2fb42eef69a077fc653
koto <2650006147@qq.com>
2022-03-31 10:24:38 +0800

    feat:增加setting页面模板

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const {loginRedirect}=require('../../middleware/loginCheck')
 
 /**
  * 获取登录信息
@@ -24,6 +25,10 @@ router.get('/login', async (ctx, next) => {
 
 router.get('/register', async (ctx, next) => {
     await ctx.render('register', {})
+})
+
+router.get('/setting', loginRedirect,async (ctx, next) => {
+    await ctx.render('setting',{ctx.session.userInfo})
 })
 
 module.exports=router
