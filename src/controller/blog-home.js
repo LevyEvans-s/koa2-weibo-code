c9a25668faf42ca70358d24b6800f8914c2fcc23
koto <2650006147@qq.com>
2022-04-01 16:20:56 +0800

    feat:分析并创建@关系

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -5,16 +5,43 @@ const xss=require('xss')
 const { createBlogFailInfo } = require('../Model/ErrorInfo')
 const { SuccessModel, ErrorModel } = require('../Model/ResModel')
 const {createBlog, getFollowersBlogList}=require('../services/blog')
-const {PAGE_SIZE}=require('../config/constant')
+const {PAGE_SIZE,REG_FOR_AT_WHO}=require('../config/constant')
+const { getUserInfo } = require('../services/user')
+const { createAtRelation } = require('../services/at-relation')
 
-//创建微博
 async function create({ userId, content, image }) {
+    // 分析并收集 content 中的 @ 用户
+    // content 格式如 '哈喽 @李四 - lisi 你好 @王五 - wangwu '
+    const atUserNameList = []
+    content = content.replace(
+        REG_FOR_AT_WHO,
+        (matchStr, nickName, userName) => {
+            // 目的不是 replace 而是获取 userName
+            atUserNameList.push(userName)
+            return matchStr // 替换不生效，预期
+        }
+    )
+
+    // 根据 @ 用户名查询用户信息
+    const atUserList = await Promise.all(
+        atUserNameList.map(userName => getUserInfo(userName))
+    )
+
+    // 根据用户信息，获取用户 id
+    const atUserIdList = atUserList.map(user => user.id)//存储的都是被@的用户
+
     try {
-        const blog = createBlog({
+        //创建微博
+        const blog = await createBlog({
             userId,
             content:xss(content),
             image
         })
+         // 创建 @ 关系
+         await Promise.all(atUserIdList.map(
+            userId => createAtRelation(blog.id, userId)
+        ))
+
         return new SuccessModel(blog)
     } catch (e) {
         console.error(e.message, e.stack)
