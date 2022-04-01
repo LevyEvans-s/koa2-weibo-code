a0b488a7ff2043f752cfe972b10fa124a317e42f
koto <2650006147@qq.com>
2022-04-01 16:58:59 +0800

    feat:标记为已读

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const { isProd } = require('./utils/env')
 const {SESSION_SECRET_KEY}=require('./config/secretKey')
 
 //路由
+const atAPIRouter=require('./routes/api/blog-at')
 const squareAPIRouter=require('./routes/api/blog-square')
 const profileAPIRouter=require('./routes/api/blog-profile')
 const blogHomeAPIRouter=require('./routes/api/blog-home')
@@ -61,6 +62,7 @@ app.use(session({
 }))
 
 // routes
+app.use(atAPIRouter.routes(),atAPIRouter.allowedMethods())
 app.use(squareAPIRouter.routes(),squareAPIRouter.allowedMethods())
 app.use(profileAPIRouter.routes(),profileAPIRouter.allowedMethods())
 app.use(blogHomeAPIRouter.routes(),blogHomeAPIRouter.allowedMethods())
