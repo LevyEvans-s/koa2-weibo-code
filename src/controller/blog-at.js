65575505b2e6797ef9bbdcdec07188679c4c6c1e
koto <2650006147@qq.com>
2022-04-01 16:55:22 +0800

    feat:开发atMe页面

--- src/controller/blog-at.js
+++ src/controller/blog-at.js
@@ -2,8 +2,14 @@
  * @description 微博@关系
  */
 
-const { SuccessModel } = require('../Model/ResModel')
-const { getAtRelationCount } = require('../services/at-relation')
+ const {
+    getAtRelationCount,
+    getAtUserBlogList,
+    updateAtRelation
+} = require('../services/at-relation')
+const { SuccessModel, ErrorModel } = require('../model/ResModel')
+const { PAGE_SIZE } = require('../config/constant')
+
 
 //获取@我的微博数量
 async function getAtMeCount(userId) {
@@ -13,6 +19,47 @@ async function getAtMeCount(userId) {
     })
 }
 
+/**
+ * 获取 @ 用户的微博列表
+ * @param {number} userId user id
+ * @param {number} pageIndex page index
+ */
+ async function getAtMeBlogList(userId, pageIndex = 0) {
+    const result = await getAtUserBlogList({
+        userId,
+        pageIndex,
+        pageSize: PAGE_SIZE
+    })
+    const { count, blogList } = result
+
+    // 返回
+    return new SuccessModel({
+        isEmpty: blogList.length === 0,
+        blogList,
+        pageSize: PAGE_SIZE,
+        pageIndex,
+        count
+    })
+}
+
+/**
+ * 标记为已读
+ * @param {number} userId userId
+ */
+async function markAsRead(userId) {
+    try {
+        await updateAtRelation(
+            { newIsRead: true },
+            { userId, isRead: false }
+        )
+    } catch (ex) {
+        console.error(ex)
+    }
+
+    // 不需要返回 SuccessModel 或者 ErrorModel
+}
 module.exports = {
-    getAtMeCount
+    getAtMeCount,
+    getAtMeBlogList,
+    markAsRead
 }
